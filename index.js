e62419f4b6f1ed32cab1628edf6d9a7f9519498a
Deepak <ds467625@gmail.com>
2022-01-27 15:02:15 +0530

    file chunking

--- index.js
+++ index.js
@@ -24,7 +24,38 @@ app.use(cors());
 
 
 app.post('/napi/uploadfile', function (req, res) {
-   res.send(req.headers);
+   console.log(req.headers);
+   if (request.headers['x-file-name']) {
+        
+    // Temporary location of our uploaded file
+    // Nginx uses a private file path in /tmp on Centos
+    // we need to get the name of that path
+    var temp_dir = fs.readdirSync('/tmp');
+    var nginx_temp_dir = [];
+    for (var i = 0; i < temp_dir.length; i++) {
+        
+        if (temp_dir[i].match('nginx.service')) {
+            nginx_temp_dir.push(temp_dir[i]);
+        }
+    }
+    
+    var temp_path = '/tmp/' + nginx_temp_dir[0] + request.headers['x-file-name'];
+    
+    fs.move(temp_path , response.locals.localfilepath, {}, function (err) {
+        
+        if (err) {
+            response.status(500).send(err);
+            return;
+        }
+        
+        // Send back a sucessful response with the file name
+        response.status(200).send(response.locals.localfilepath);
+        response.end();
+                
+            
+    });
+}
+  
   })
 
 
